8589ea67383e4f8d2f1cf48c0fb9f73470e585e4
jokernauten <jokernauten@gmail.com>
2022-08-19 16:16:54 +0400

    update

--- ecr/main.tf
+++ ecr/main.tf
@@ -9,3 +9,26 @@ provider "aws" {
 resource "aws_ecr_repository" "its-ecr" {
   name = var.ecr_name
 }
+
+resource "aws_ecr_repository_policy" "its-ecr-policy" {
+  repository = aws_ecr_repository.its-ecr.name
+
+  policy = <<EOF
+{
+    "Version": "2008-10-17",
+    "Statement": [
+        {
+            "Sid": "new policy",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": [
+                "ecr:UploadLayerPart",
+                "ecr:InitiateLayerUpload",
+                "ecr:CompleteLayerUpload"
+                "ecr:PutImage"
+            ]
+        }
+    ]
+}
+EOF
+}
