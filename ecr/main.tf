84aedc356ccbed4cfccd96ad984a28dc9d440a8e
jokernauten <jokernauten@gmail.com>
2022-08-19 16:36:31 +0400

    update

--- ecr/main.tf
+++ ecr/main.tf
@@ -18,14 +18,24 @@ resource "aws_ecr_repository_policy" "its-ecr-policy" {
     "Version": "2008-10-17",
     "Statement": [
         {
-            "Sid": "new policy",
+            "Sid": "push policy",
             "Effect": "Allow",
             "Principal": "*",
             "Action": [
-                "ecr:UploadLayerPart",
+                "ecr:GetDownloadUrlForLayer",
+                "ecr:BatchGetImage",
+                "ecr:BatchCheckLayerAvailability",
+                "ecr:PutImage",
                 "ecr:InitiateLayerUpload",
+                "ecr:UploadLayerPart",
                 "ecr:CompleteLayerUpload",
-                "ecr:PutImage"
+                "ecr:DescribeRepositories",
+                "ecr:GetRepositoryPolicy",
+                "ecr:ListImages",
+                "ecr:DeleteRepository",
+                "ecr:BatchDeleteImage",
+                "ecr:SetRepositoryPolicy",
+                "ecr:DeleteRepositoryPolicy"
             ]
         }
     ]
