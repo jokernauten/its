258cd86edbc596356d6496cec26bd574a9385a30
jokernauten <jokernauten@gmail.com>
2022-08-20 17:46:13 +0400

    update

---
+++ its-testtask-node/terragrunt/efs/main.tf
@@ -0,0 +1,17 @@
+terraform {
+    backend "s3" {}
+}
+
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_efs_file_system" "efs" {
+   creation_token = "efs"
+   performance_mode = var.performance_mode
+   throughput_mode = var.throughput_mode
+   encrypted = "true"
+ tags = {
+     Name = var.efs_name
+   }
+ }
---
+++ its-testtask-node/terragrunt/efs/variables.tf
@@ -0,0 +1,8 @@
+variable "efs_name" {
+}
+variable "throughput_mode" {
+}
+
+variable "performance_mode" {
+  
+}
